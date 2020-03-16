2e98211c1859fb5744ba189e0d23ffc0d05fdc96
tock <2966497308@qq.com>
2020-03-16 19:47:02 +0800

    更新代码！

---
+++ NakitHowHuade/NakitToolsHeader.h
@@ -0,0 +1,36 @@
+//
+//  NakitToolsHeader.h
+//  NakitHowHuade
+//
+//  Created by admin on 16/3/2020.
+//  Copyright © 2020 howhuade. All rights reserved.
+//
+
+#ifndef NakitToolsHeader_h
+#define NakitToolsHeader_h
+
+#define APP_ID @"BfhCX7hNFtkQ0DlW79M5kEqp-gzGzoHsz"
+#define APP_KEY @"cXIinf17uf4G1he7tSxzPzwb"
+#define API_URL @"https://bfhcx7hn.lc-cn-n1-shared.com"
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+#define BarHight \
+({\
+CGRect rectStatus = [[UIApplication sharedApplication] statusBarFrame];\
+CGRect rectNav = self.navigationController.navigationBar.frame;\
+( rectStatus.size.height+ rectNav.size.height);\
+})\
+
+
+typedef NS_ENUM(UInt32, UIColorRGB) {
+       appColor         = 0xD44000,
+
+};
+
+#endif /* NakitToolsHeader_h */
--- NakitHowHuade/ViewController.m
+++ NakitHowHuade/ViewController.m
@@ -1,12 +1,13 @@
 //
 //  ViewController.m
-//  NakitHowHuade
+//  NakitHowHuade 
 //
 //  Created by admin on 16/3/2020.
 //  Copyright © 2020 howhuade. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "NakitToolsHeader.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIImageView *imgView = [[UIImageView alloc] init];
+       imgView.contentMode =     UIViewContentModeScaleAspectFill;
+       imgView.image = [UIImage imageNamed:@"launch"];
+       imgView.frame = [UIScreen mainScreen].bounds;
+       [self.view addSubview:imgView];
+    
+    UIScrollView *PEHIVURSheSC = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH , SCREEN_HEIGHT)];
+    PEHIVURSheSC.contentSize = CGSizeMake(PEHIVURSheSC.frame.size.width,PEHIVURSheSC.frame.size.height + 1);
+    PEHIVURSheSC.backgroundColor = [UIColor clearColor];
+    PEHIVURSheSC.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:PEHIVURSheSC];
+    
+    
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"animal.plist" ofType:nil];
+    NSArray *a = [[NSArray alloc]initWithContentsOfFile:filePath];
+    NSMutableArray*dataSource = [NSMutableArray array];
+    
+    for (NSDictionary *dic in a) {
+            NSString *name = dic[@"name"];
+               
+        [dataSource addObject:name];
+               //NSString *fileName = [NSString stringWithFormat:@"%@@2x.png",name];
+            
+    }
+    
+    
+      NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"resources" ofType:@"bundle"];
+    
+    for (NSInteger i = 0; i < dataSource.count; i++) {
+        UIImageView *PEHIVURSheIV = [[UIImageView alloc]initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width/3 * (i%3), [UIScreen mainScreen].bounds.size.width/3 * (i/3), [UIScreen mainScreen].bounds.size.width/3 , [UIScreen mainScreen].bounds.size.width/3 )];
+
+         
+        NSLog(@"=====================%@",dataSource[i]);
+        
+          NSString *name = dataSource[i];
+
+         NSString *fileName = [[bundlePath stringByAppendingPathComponent:@"animal"] stringByAppendingPathComponent:name];
+         [PEHIVURSheIV setImage:[UIImage imageWithContentsOfFile:fileName]];
+        [PEHIVURSheSC addSubview:PEHIVURSheIV];
+        PEHIVURSheIV.tag = i;
+        PEHIVURSheIV.userInteractionEnabled = YES;
+       // [PEHIVURSheIV addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(PEHIVURSheTap:)]];
+        
+        if (PEHIVURSheIV.frame.origin.y + PEHIVURSheIV.frame.size.height > PEHIVURSheSC.contentSize.height) {
+            PEHIVURSheSC.contentSize = CGSizeMake(PEHIVURSheSC.frame.size.width,PEHIVURSheIV.frame.origin.y + PEHIVURSheIV.frame.size.height + 1);
+        }
+    }
 }
 
 
