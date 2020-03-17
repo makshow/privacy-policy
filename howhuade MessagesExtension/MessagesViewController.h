7128c3ce9bd9429f823874a142c0daefb7886935
tock <2966497308@qq.com>
2020-03-17 16:20:29 +0800

    更新代码！

---
+++ howhuade MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  howhuade MessagesExtension
+//
+//  Created by admin on 17/3/2020.
+//  Copyright © 2020 howhuade. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
