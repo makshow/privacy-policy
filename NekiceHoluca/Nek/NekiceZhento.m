316976ef8dc4e37d5122c0c9e060e5e0cb04b0a0
tock <2966497308@qq.com>
2020-03-19 16:37:41 +0800

    更新接口

--- NekiceHoluca/Nek/NekiceZhento.m
+++ NekiceHoluca/Nek/NekiceZhento.m
@@ -31,6 +31,8 @@
 #pragma mark - Navigation
 //https://sweetcandy.w.eolinker.com/#/home/ams/project/inside/api/list?groupID=-1&projectHashKey=v6CRCcTde0e0448976e4f45e4c03b5dfc1ec40290d21629&spaceKey=sweetcandy
 //https://www.leancloud.cn
+ //https://mockapi.eolinker.com/v6CRCcTde0e0448976e4f45e4c03b5dfc1ec40290d21629/api/index/const
+ 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
